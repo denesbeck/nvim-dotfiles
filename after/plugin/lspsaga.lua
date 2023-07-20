1bab2722c759b16474911f1a73b3af7511cc1459
Denes Beck <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>
2023-07-20 20:43:39 +0200

    lspsaga

--- after/plugin/lspsaga.lua
+++ after/plugin/lspsaga.lua
@@ -1,29 +1,13 @@
-local ok, saga = pcall(require, "lspsaga")
+local ok, lspsaga = pcall(require, "lspsaga")
 if (not ok) then return end
 
-saga.setup({
-  ui = {
-    winblend = 10,
-    border = 'rounded',
-    colors = {
-      normal_bg = '#002b36'
-    }
-  },
-  symbol_in_winbar = {
-    enable = false
-  }
-})
+lspsaga.setup {}
 
-local diagnostic = require("lspsaga.diagnostic")
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
-vim.keymap.set('n', 'gl', '<Cmd>Lspsaga show_line_diagnostics<CR>', opts)
-vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
-vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
--- vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
-vim.keymap.set('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
-vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
-
--- code action
-vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+vim.keymap.set('n', '<leader>pd', '<Cmd>Lspsaga peek_definition<CR>')
+vim.keymap.set('n', '<leader>jn', '<Cmd>Lspsaga diagnostic_jump_next<CR>')
+vim.keymap.set('n', '<leader>jp', '<Cmd>Lspsaga diagnostic_jump_next<CR>')
+vim.keymap.set('n', '<leader>lf', '<Cmd>Lspsaga finder<CR>')
+vim.keymap.set('n', '<leader>tt', '<Cmd>Lspsaga term_toggle<CR>')
+vim.keymap.set('n', '<leader>ld', '<Cmd>Lspsaga show_line_diagnostics<CR>')
+vim.keymap.set('n', '<leader>hd', '<Cmd>Lspsaga hover_doc<CR>')
+vim.keymap.set('n', '<leader>rn', '<Cmd>Lspsaga rename<CR>')
