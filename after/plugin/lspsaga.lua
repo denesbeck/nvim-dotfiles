adfbb88bbdad6cc374355395470cf31b02595b75
Denes Beck <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>
2023-07-21 17:05:40 +0200

    opts added lspsaga

--- after/plugin/lspsaga.lua
+++ after/plugin/lspsaga.lua
@@ -3,11 +3,13 @@ if (not ok) then return end
 
 lspsaga.setup {}
 
-vim.keymap.set('n', '<leader>pd', '<Cmd>Lspsaga peek_definition<CR>')
-vim.keymap.set('n', '<leader>jn', '<Cmd>Lspsaga diagnostic_jump_next<CR>')
-vim.keymap.set('n', '<leader>jp', '<Cmd>Lspsaga diagnostic_jump_next<CR>')
-vim.keymap.set('n', '<leader>lf', '<Cmd>Lspsaga finder<CR>')
-vim.keymap.set('n', '<leader>tt', '<Cmd>Lspsaga term_toggle<CR>')
-vim.keymap.set('n', '<leader>ld', '<Cmd>Lspsaga show_line_diagnostics<CR>')
-vim.keymap.set('n', '<leader>hd', '<Cmd>Lspsaga hover_doc<CR>')
-vim.keymap.set('n', '<leader>rn', '<Cmd>Lspsaga rename<CR>')
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set('n', '<leader>pd', '<Cmd>Lspsaga peek_definition<CR>', opts)
+vim.keymap.set('n', '<leader>jn', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', '<leader>jp', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', '<leader>fd', '<Cmd>Lspsaga finder<CR>', opts)
+vim.keymap.set('n', '<leader>tt', '<Cmd>Lspsaga term_toggle<CR>', opts)
+vim.keymap.set('n', '<leader>ld', '<Cmd>Lspsaga show_line_diagnostics<CR>', opts)
+vim.keymap.set('n', '<leader>hd', '<Cmd>Lspsaga hover_doc<CR>', opts)
+vim.keymap.set('n', '<leader>rn', '<Cmd>Lspsaga rename<CR>', opts)
