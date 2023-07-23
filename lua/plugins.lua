77a0340aa5c83de2d8d6ac43948e5d2da4fe239e
Denes Beck <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>
2023-07-23 17:34:02 +0200

    small changes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -23,14 +23,14 @@ local plugins = {
     lazy = false,
     priority = 1000,
     opts = {
-      transparent=true,
+      transparent = true,
       styles = {
           sidebars = "transparent",
           floats = "transparent",
       },
     },
     config = function()
-      vim.cmd([[colorscheme tokyonight]])
+      vim.cmd([[colorscheme tokyonight-night]])
     end,
   },
   
