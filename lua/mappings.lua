d8fc8866800249bf0a04989e0d04cdb8b15f4209
Denes Beck <denes.beck1@outlook.com>
2023-09-25 20:24:53 +0200

    split window keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -17,6 +17,23 @@ keymap.set("n", "<leader>c", "<cmd>bw<CR>")
 -- Reset search highlight
 keymap.set("n", "//", "<cmd>nohlsearch<CR>")
 
+-- Split window
+keymap.set('n', 'sh', ':split<Return><C-w>w')
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
+-- Move window
+keymap.set('n', '<Space>', '<C-w>w')
+keymap.set('', '<C-h>', '<C-w>h')
+keymap.set('', '<C-k>', '<C-w>k')
+keymap.set('', '<C-j>', '<C-w>j')
+keymap.set('', '<C-l>', '<C-w>l')
+
+-- Resize window
+keymap.set('n', '<C-S-h>', '<C-w><')
+keymap.set('n', '<C-S-l>', '<C-w>>')
+keymap.set('n', '<C-S-k>', '<C-w>+')
+keymap.set('n', '<C-S-j>', '<C-w>-')
+
 -- Insert --
 -- Leave insert mode
 keymap.set("i", "jj", "<ESC>")
