85d1c66b3d152d2015b12fe4f1964ad32cef104d
Denes Beck <denes.beck1@outlook.com>
2023-10-01 17:36:52 +0200

    keymap adjustment for window splits

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -17,22 +17,26 @@ keymap.set("n", "<leader>c", "<cmd>bw<CR>")
 -- Reset search highlight
 keymap.set("n", "//", "<cmd>nohlsearch<CR>")
 
--- Split window
+-- Create window split
 keymap.set('n', 'sh', ':split<Return><C-w>w')
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
 
--- Move window
+-- Move around splits
 keymap.set('n', '<Space>', '<C-w>w')
 keymap.set('', '<C-h>', '<cmd>TmuxNavigateLeft<CR>')
 keymap.set('', '<C-k>', '<cmd>TmuxNavigateUp<CR>')
 keymap.set('', '<C-j>', '<cmd>TmuxNavigateDown<CR>')
 keymap.set('', '<C-l>', '<cmd>TmuxNavigateRight<CR>')
 
--- Resize window
+-- Resize split
 keymap.set('n', '<M-h>', '<C-w><')
 keymap.set('n', '<M-l>', '<C-w>>')
 keymap.set('n', '<M-k>', '<C-w>+')
 keymap.set('n', '<M-j>', '<C-w>-')
+
+-- Close split
+keymap.set('n', '<leader>cc', '<C-w>c')
+keymap.set('n', '<leader>oo', '<C-w>o')
 
 -- Insert --
 -- Leave insert mode
